8802d4604def08e3ade825dc9f43125346f7ef38
Meng-Gen <plover@gmail.com>
2013-05-01 18:10:41 +0800

    Format tabbing

--- UVa/acm_458.cc
+++ UVa/acm_458.cc
@@ -4,14 +4,14 @@ char buffer[50000];
 
 int main(int argc, char* argv[])
 {
-    while(std::cin.getline(buffer, 50000))
-    {
+	while(std::cin.getline(buffer, 50000))
+	{
 		for (int i = 0; buffer[i] != 0; i++)
 		{
 			// '*' - '1' is the integer -7.
 			std::cout << (char)(buffer[i] - 7);
 		}
 		std::cout << std::endl;
-    }
-    return 0;
+	}
+	return 0;
 }
