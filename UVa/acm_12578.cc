c89e185cf081e95d5b77b233c649768235ab7a28
Meng-Gen Tsai <menggen_tsai@tw-menggentsai1.tw.trendnet.org>
2013-05-02 18:36:00 +0800

    Easy math

---
+++ UVa/acm_12578.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+
+#define CONSTANT_PI acos(-1.0);
+
+int main(int argc, char* argv[])
+{
+    int n;
+    while(std::cin >> n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            int flag_length;
+            std::cin >> flag_length;
+            
+			double red_area = (double)flag_length * (double)flag_length * 0.2 * 0.2 * CONSTANT_PI;
+			double green_area = (double)flag_length * (double)flag_length * 0.6 - red_area;
+			printf("%.2lf %.2lf\n", red_area, green_area);
+        }
+    }
+
+    return 0;
+}
