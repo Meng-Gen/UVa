967d5ef4c0b8c235b79f33e89d81eeb3e4f3eb14
Meng-Gen Tsai <menggen_tsai@tw-menggentsai1.tw.trendnet.org>
2013-05-06 11:11:39 +0800

    Easy practice of C++ STL map

--- UVa/acm_12250.cc
+++ UVa/acm_12250.cc
@@ -1,16 +1,33 @@
 #include <iostream>
+#include <map>
 #include <string>
 #include <stdio.h>
 
-int query[5001][2000] = {};
-
 int main(int argc, char* argv[])
 {
-    std::string input_string;
+    std::map<std::string, std::string> language_detection_map;
+    language_detection_map["HELLO"] = "ENGLISH";
+    language_detection_map["HOLA"] = "SPANISH";
+    language_detection_map["HALLO"] = "GERMAN";
+    language_detection_map["BONJOUR"] = "FRENCH";
+    language_detection_map["CIAO"] = "ITALIAN";
+    language_detection_map["ZDRAVSTVUJTE"] = "RUSSIAN";
+
     int case_id = 1;
-    while (std::cin >> input_string)
+    std::string hello;
+    while (std::cin >> hello)
     {
-        std::cout <<
+        if (hello.compare("#") == 0)
+        {
+            break;
+        }
+
+        std::string language = language_detection_map[hello];
+        if (language.empty())
+        {
+            language = "UNKNOWN";
+        }
+        std::cout << "Case " << case_id << ": " << language << std::endl;
 
         case_id++;
     }
