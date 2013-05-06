f5c332c9754a3f72fec9154cbe011dae39f7e6ad
Meng-Gen Tsai <menggen_tsai@tw-menggentsai1.tw.trendnet.org>
2013-05-06 13:34:29 +0800

    Format tabbing

--- UVa/acm_272.cc
+++ UVa/acm_272.cc
@@ -6,23 +6,23 @@ char buffer[50000];
 
 int main(int argc, char* argv[])
 {
-	bool begin_quotation = true;
+    bool begin_quotation = true;
     while(std::cin.getline(buffer, 50000))
     {
-		std::stringstream string_builder;
-		for (int i = 0; buffer[i] != 0; i++)
-		{
-			if (buffer[i] == '"')
-			{
-				string_builder << (begin_quotation ? "``" : "''");
-				begin_quotation = !begin_quotation;
-			}
-			else
-			{
-				string_builder << buffer[i];
-			}
-		}
-		std::cout << string_builder.str() << std::endl;
+        std::stringstream string_builder;
+        for (int i = 0; buffer[i] != 0; i++)
+        {
+            if (buffer[i] == '"')
+            {
+                string_builder << (begin_quotation ? "``" : "''");
+                begin_quotation = !begin_quotation;
+            }
+            else
+            {
+                string_builder << buffer[i];
+            }
+        }
+        std::cout << string_builder.str() << std::endl;
     }
 
     return 0;
